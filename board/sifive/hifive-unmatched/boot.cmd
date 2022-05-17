5071e3b25f66c1ec03e41cb787e172249a99a3ce
Wang Junqiang <wangjunqiang@iscas.ac.cn>
2022-05-17 15:05:09 +0800

    fix for rvlab

--- board/sifive/hifive-unmatched/boot.cmd
+++ board/sifive/hifive-unmatched/boot.cmd
@@ -1,4 +1,10 @@
-setenv bootargs earlycon=sbi root=/dev/mmcblk0p3 rootwait
-load mmc 0:3 ${kernel_addr_r} boot/Image
-load mmc 0:3 ${fdt_addr_r} boot/hifive-unmatched-a00.dtb
-booti ${kernel_addr_r} - ${fdt_addr_r}
+if test -e mmc 0:3 boot/uEnv.txt ; then
+    load mmc 0:3 ${kernel_addr_r} /boot/uEnv.txt
+    env import -t $kernel_addr_r $filesize
+    run tftpboot
+else
+    setenv bootargs earlycon=sbi root=/dev/mmcblk0p3 rootwait
+    load mmc 0:3 ${kernel_addr_r} boot/Image
+    load mmc 0:3 ${fdt_addr_r} boot/hifive-unmatched-a00.dtb
+    booti ${kernel_addr_r} - ${fdt_addr_r}
+fi
